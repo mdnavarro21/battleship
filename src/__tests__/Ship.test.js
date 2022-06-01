e3a69ceb62bbfb01d67519997f527e294caed880
Matthew Dylan <dylannavarro65@gmail.com>
2022-06-01 12:15:17 -0400

    implemented Ship module and testing

--- src/__tests__/Ship.test.js
+++ src/__tests__/Ship.test.js
@@ -1,6 +1,49 @@
 import ShipFactory from "../modules/Ship";
 
-test("testing test", () => {
-  const newShip = ShipFactory(5);
-  expect(newShip.isSunk()).toBe(false);
+describe('Ship properties initialized correctly', ()=> {
+  const newShip = ShipFactory(4);
+  test('returns length of battleship = 4', () => {
+    expect(newShip.getLength()).toBe(4);  
+  });
+
+  test('Throw error if no length specified', () => {
+    expect(() => ShipFactory()).toThrow('No length specified');
+  });
+
+  test('returns null array of length 4 for battleship', () => {
+    expect(newShip.getHits()).toEqual([null, null, null, null]);
+  });  
+
+  test('isSunk function - expected false', () => {
+    expect(newShip.isSunk()).toBeFalsy();
+  })
 });
+
+
+test('Hit function happy path', () => {
+  const newShip = ShipFactory(5);
+  newShip.hit(2);
+  expect(newShip.getHits()).toEqual([null,null,'hit',null,null]);
+});
+
+test('Hit function - index out of range', () => {
+  const newShip = ShipFactory(3);
+  expect(()=> newShip.hit(4)).toThrow('Ship index out of range');
+});
+
+test('isSunk happy path 1 - ship still afloat after hits; expecting false', () => {
+  const newShip = ShipFactory(4);
+  for (let i = 0; i < newShip.getLength(); i += 2) {
+    newShip.hit(i);
+  }
+  expect(newShip.isSunk()).toBeFalsy();
+});
+
+test('isSunk happy path 2 - ship has sunk', () => {
+  const newShip = ShipFactory(4);
+  for (let i = 0; i < newShip.getLength(); i += 1) {
+    newShip.hit(i);
+  }
+  expect(newShip.isSunk()).toBeTruthy();
+});
+
--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,19 +1,24 @@
 const ShipFactory = (length) => {
-  let sunk = false;
-  const hits = new Array(length).fill("o");
+  if (!length) {
+    throw new Error("No length specified");
+  }
+  let hits = new Array(length).fill(null);
+
+  const getHits = () => hits;
+  const getLength = () => length;
 
   const hit = (index) => {
+    if (index >= length || index < 0) {
+      throw new Error('Ship index out of range');
+    }
     hits[index] = "hit";
-  };
+  }
 
   const isSunk = () => {
-    if (hits.every((element) => element === "hit")) {
-      sunk = true;
-    }
-    return sunk;
-  };
+    return hits.every((tile) => tile == 'hit');
+  }
 
-  return { hit, isSunk };
+  return {getLength, getHits, hit, isSunk}
 };
 
 export default ShipFactory;
