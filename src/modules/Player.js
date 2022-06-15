c102130620c75bc566e131769ad4d380d7112308
Matthew Dylan <dylannavarro65@gmail.com>
2022-06-15 13:06:21 -0400

    Added player ID property

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,3 +1,22 @@
-const PlayerFactory = () => {};
+import GameboardFactory from "./Gameboard";
+import ShipFactory from "./Ship";
+
+const PlayerFactory = (player_id) => {
+    const id = player_id;
+    let gameBoard = GameboardFactory();
+    let ships = [ShipFactory(5), ShipFactory(4), ShipFactory(3), ShipFactory(2), ShipFactory(1)];
+
+    const attack = (opponentBoard, coordinates) => {
+        return opponentBoard.receiveAttack(coordinates);
+    }
+
+    
+    return { 
+        get gameBoard(){ return gameBoard },
+        get ships() { return ships },
+        get id() { return id},
+        attack
+    }
+};
 
 export default PlayerFactory;
