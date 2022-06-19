6e026c4534f1accf15e5c4a23cf746bafe432590
Matthew Dylan <dylannavarro65@gmail.com>
2022-06-19 16:07:02 -0400

    removed ships array from Player object

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,10 +1,8 @@
 import GameboardFactory from "./Gameboard";
-import ShipFactory from "./Ship";
 
 const PlayerFactory = (player_id) => {
     const id = player_id;
     let gameBoard = GameboardFactory();
-    let ships = [ShipFactory(5), ShipFactory(4), ShipFactory(3), ShipFactory(2), ShipFactory(1)];
 
     const attack = (opponentBoard, coordinates) => {
         return opponentBoard.receiveAttack(coordinates);
@@ -13,7 +11,6 @@ const PlayerFactory = (player_id) => {
     
     return { 
         get gameBoard(){ return gameBoard },
-        get ships() { return ships },
         get id() { return id},
         attack
     }
